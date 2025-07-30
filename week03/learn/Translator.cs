e1180d9900c2d2c97e816c8c7cdbec3dac319516
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-07-30 17:02:50 -0500

    "Update Translator.cs: Replace AddWord method implementation"

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -24,7 +24,7 @@ public class Translator
     /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord] = toWord;
     }
 
     /// <summary>
